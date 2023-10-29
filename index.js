ebbece13b306f9bfef4cca94330c8f459d487781
Pichet Suksai <pichet.suksai@7peakssoftware>
2023-10-30 01:40:09 +0700

    install lowdb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { LowSync } = require('lowdb/lib');
 const app = express();
 
 // Basic Configuration
@@ -17,6 +18,25 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+// Developed function
+app.get('/api/test', function(req, res) {
+  const title = "This is a test";
+  const adapter = new JSONFileSync('file.json');
+  //const db = new LowSync(adapter)
+  const db = new LowSync(adapter);
+
+  db.read();  //Load the content of the file into memory
+  db.data ||= { posts: [] }; //default value
+
+  db.data.posts.push({ title }); //add data into the "collection"
+
+  db.write() //persist the data by saving it to the JSON file
+
+  //any Find-like operation is left to the skill of the user
+
+  let record = db.data.posts.find( p => p.title == "Hello world");
 });
 
 app.listen(port, function() {
