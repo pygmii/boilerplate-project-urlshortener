21618530001e7682fbe5590e47fa8a7a88e8f074
Pichet Suksai <pichet.suksai@7peakssoftware>
2023-10-30 01:46:32 +0700

    unuse lowdb

--- index.js
+++ index.js
@@ -22,21 +22,7 @@ app.get('/api/hello', function(req, res) {
 
 // Developed function
 app.get('/api/test', function(req, res) {
-  const title = "This is a test";
-  const adapter = new JSONFileSync('file.json');
-  //const db = new LowSync(adapter)
-  const db = new LowSync(adapter);
 
-  db.read();  //Load the content of the file into memory
-  db.data ||= { posts: [] }; //default value
-
-  db.data.posts.push({ title }); //add data into the "collection"
-
-  db.write() //persist the data by saving it to the JSON file
-
-  //any Find-like operation is left to the skill of the user
-
-  let record = db.data.posts.find( p => p.title == "Hello world");
 });
 
 app.listen(port, function() {
